acd9185a8b004172551335b3d2d7b118cbeac6e0
deokgoo <kkddgg1001@gmail.com>
2021-08-15 00:38:12 +0900

    unitTest example end :)

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,66 @@
 import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
+// unit Test 팀내에서 test coverage
+
+const createWrapper = (msg = 'test') => shallowMount(HelloWorld, {
+  props: { msg },
+});
+
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+    const msg = 'test msg';
+    const wrapper = createWrapper(msg);
+    expect(wrapper.props('msg')).toEqual(msg);
+  });
+
+  // it('count click test', async () => {
+  //   const wrapper = createWrapper();
+  //   const plusCntSpy = jest.spyOn(wrapper.vm, 'plusCnt');
+  //   const minusCntSpy = jest.spyOn(wrapper.vm, 'minusCnt');
+  //   const loginFn = jest.fn();
+  //   jest.mock('../services/authService', () => ({
+  //     login: loginFn,
+  //     logout: loginFn,
+  //   }));
+  //   const userName = 'userName';
+  //   wrapper.vm.loginHandler(userName);
+  //   expect(loginFn).toBeCalledTimes(1);
+  //   expect(loginFn.mock.calls[0]).toEqual([userName]);
+
+  //   expect(wrapper.vm.count).toBe(0);
+
+  //   await wrapper.find('.b').trigger('click');
+  //   await wrapper.vm.$nextTick();
+  //   expect(plusCntSpy).toBeCalledTimes(1);
+  //   expect(wrapper.vm.count).toBe(1);
+
+  //   await wrapper.find('.a').trigger('click');
+  //   await wrapper.vm.$nextTick();
+  //   expect(minusCntSpy).toBeCalledTimes(1);
+  //   expect(wrapper.vm.count).toBe(0);
+  // });
+
+  it('count click test2', async () => {
+    const wrapper = createWrapper();
+    const plusCntSpy = jest.spyOn(wrapper.vm, 'plusCnt');
+    const minusCntSpy = jest.spyOn(wrapper.vm, 'plusCnt');
+
+    expect(wrapper.vm.count).toBe(0);
+
+    await wrapper.find('.b').trigger('click');
+    await wrapper.vm.$nextTick();
+    expect(plusCntSpy).toBeCalledTimes(1);
+    expect(wrapper.vm.count).toBe(1);
+
+    await wrapper.find('.a').trigger('click');
+    await wrapper.vm.$nextTick();
+    expect(minusCntSpy).toBeCalledTimes(1);
+    expect(wrapper.vm.count).toBe(0);
+
+    await wrapper.find('.a').trigger('click');
+    await wrapper.vm.$nextTick();
+    expect(minusCntSpy).toBeCalledTimes(1);
+    expect(wrapper.vm.count).toBe(0);
   });
 });

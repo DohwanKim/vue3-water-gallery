d06e802347ad5d1f170a743a343099068ca8e0ce
deokgoo <kkddgg1001@gmail.com>
2021-09-09 09:46:43 +0900

    feature: landing top

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,6 @@
 </template>
 
 <style>
+@import "assets/style/base.css";
 @import "assets/style/style.css";
-
-#app {
-  color: #2c3e50;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  padding: 0;
-  text-align: center;
-}
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,7 +1,9 @@
 <template>
-  <header>head</header>
+  <LandingHeader />
   <main>
-    <section>Landing top</section>
+    <section class="main__top">
+      <LandingTop />
+    </section>
     <section>
       <Gallery />
     </section>
@@ -12,9 +14,25 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import Gallery from "@/components/Index/Gallery.vue";
+import LandingHeader from "@/components/LandingHeader.vue";
+import LandingTop from "@/components/LandingTop.vue";
 
 export default defineComponent({
   name: "Index",
-  components: { Gallery },
+  components: {
+    Gallery,
+    LandingHeader,
+    LandingTop,
+  },
 });
 </script>
+
+<style scoped>
+@import "../assets/style/common.css";
+
+.main__top {
+  background-color: black;
+  max-height: calc(100vh - var(--size-header));
+  overflow: hidden;
+}
+</style>
